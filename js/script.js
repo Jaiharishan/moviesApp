387193c8e27f868622e29aab596dcd6c85419dc9
Jaiharishan <downdkar@gmail.com>
2021-08-21 12:41:24 +0530

    casts added

--- js/script.js
+++ js/script.js
@@ -22,54 +22,102 @@ function moviesSection(movies) {
     return movies.map(movie => {
         if (movie.poster_path) {
             
-            return `
-                    <img src=${imageUrl + movie.poster_path} data-movie-id=${movie.id}>
-                `  
+            let movieImg = document.createElement('img');
+            movieImg.src = imageUrl + movie.poster_path;
+            movieImg.setAttribute('data-movie-id', movie.id);
+            // return `
+            //         <img src=${imageUrl + movie.poster_path} data-movie-id=${movie.id}>
+            //     `  
+            return movieImg
         }
         
     })
 }
 
 
+// to use fetch api more clearly
+function fetchMovies(url, success, error) {
+    fetch(url)
+        .then(res => res.json())
+        .then(success)
+        .catch(error)
+}
+
+function errorHandle(error) {
+    console.log(error);
+}
+
+
+
+// for movie casts
 function movieCasts(movies) {
     return movies.map(movie => {
         let casts = [];
-            
+        let castsElement = document.createElement('div');
         // fetching cast details from the api
         fetch(castUrl(movie.id))
             .then(res => res.json())
             .then(data => {
-                    data.cast.forEach(movie_cast => {
-                        casts.push([movie_cast.original_name, movie_cast.character]);
 
-                });
+                    // console.log(casts);
                     
+
+                    data.cast.forEach(movie_cast => {
+                        
+                        let castinfo = document.createElement('p');
+                        castinfo.innerHTML = movie_cast.original_name + ' as ' + movie_cast.character;
+                        castsElement.appendChild(castinfo);
+                        console.log(castinfo);
+                        // casts.push([movie_cast.original_name, movie_cast.character]);
+                    });
+
+                    
+
+                    // console.log(casts);
+                    // casts.forEach(cast => {
+                    //     let castinfo = document.createElement('p');
+                    //     castinfo.innerHTML = cast[0] + ' as ' + cast[1];
+                    //     console.log(castinfo);
+                    //     castsElement.appendChild(castinfo);
+                    // });
+
+
             })
             .catch(err => console.log(err));
-            
-        // console.log(casts);
-        let castsElement = document.createElement('div');
         
-
-        casts.forEach(cast => {
-            let castinfo = document.createElement('p');
-            castinfo.innerHTML = cast[0] + ' as ' + cast[1];
-            castsElement.appendChild(castinfo);
-        });
-
+            
         return castsElement;
+            
+        
     })
         
 }
 
 
+// this function is to create movie element
 function createMovies(movies) {
     const movieElement = document.createElement('div');
     movieElement.setAttribute('class', 'movie');
 
+    // the section where all searched movies placed
     let moviesSectionElm = document.createElement('section');
     moviesSectionElm.setAttribute('class', 'section');
-    moviesSectionElm.innerHTML = `${moviesSection(movies)}`;
+    let movieImgs = moviesSection(movies);
+    let moviesCasts = movieCasts(movies);
+
+    // console.log(movieImgs);
+    // console.log(moviesCasts);
+
+    for (let i = 0; i < movieImgs.length; i++) {
+        let movieDiv = document.createElement('div');
+        movieDiv.appendChild(movieImgs[i]);
+        movieDiv.appendChild(moviesCasts[i]);
+        moviesSectionElm.appendChild(movieDiv);
+    }
+    // movieImgs.forEach(movieImg => {
+    //     moviesSectionElm.appendChild(movieImg)
+    // })
+
     // console.log(movieCasts(movies));
 
     movieElement.appendChild(moviesSectionElm);
@@ -77,6 +125,7 @@ function createMovies(movies) {
 }
 
 
+// to render the searched movies
 function searchAndGenerateMovies(data) {
     // removing already existing movies
     moviesSearchContainer.innerHTML = '';
@@ -87,19 +136,25 @@ function searchAndGenerateMovies(data) {
     moviesSearchContainer.appendChild(movies);
 }
 
+
+
+// the search function which handles api calls to render movie in website
+function searchMovies(value) {
+    // const path = '/search/movie';
+    const ourUrl = url + '&query=' + value;
+    fetchMovies(ourUrl, searchAndGenerateMovies, errorHandle)
+
+}
+
+
+
 searchButton.addEventListener('click', (e) => {
     e.preventDefault();
     console.log(moviesInput.value);
     const value = moviesInput.value;
 
-    const ourUrl = url + '&query=' + value
-
-    fetch(ourUrl)
-        .then((res) => res.json())
-        .then((data) => {
-            searchAndGenerateMovies(data)
-        })
-        .catch((err) => console.log(err))
+    
+    searchMovies(value);
 
     // emptying the search bar
     moviesInput.value = '';
